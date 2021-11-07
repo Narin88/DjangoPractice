c4ce46e6de9386f283d0c9f42d0d89d0706c079b
YoonSeul88 <dlrjatk1@gmail.com>
2021-11-07 20:07:12 +0900

    djust Django course 8 commit

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'pinterest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -89,6 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': 'localhost:1521/orclpdb',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '1521',
         'USER': 'python',
         'PASSWORD': '1234',
     }
