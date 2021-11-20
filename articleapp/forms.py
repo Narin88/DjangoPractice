a33095d19d82e8a14f8f7f498be98f295193af6c
YoonSeul88 <dlrjatk1@gmail.com>
2021-11-20 14:19:23 +0900

    django course 44 medium Editor 사용, WYSIWYG

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,7 +1,14 @@
+from django import forms
 from django.forms import ModelForm
-from .models import Article
+
+from .models import Article, Project
 
 class ArticleCreationForm(ModelForm):
+  # content field가 만들어질때 먼저설정하는 class style
+  content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left', 'style': 'height: auto;'}))
+
+  project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
   class Meta:
     model = Article
     fields = ['title', 'image', 'project', 'content']
